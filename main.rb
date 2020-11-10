62f4a2488c9789db599545d0c8d80036e8b7ee41
Henok <henatan99@gmail.com>
2020-11-11 01:26:51 +0300

    Fix file conflict

--- main.rb
+++ main.rb
@@ -27,3 +27,19 @@ def bubble_sort_by(array)
   end
   array
 end
+
+def bubble_sort_by(array)
+  n = array.length
+  loop do
+    swap = false
+    (n - 1).times do |i|
+      diff = yield(array[i], array[i+1])
+      if diff > 0
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swap = true
+      end
+    end
+    break unless swap
+  end
+  array
+end

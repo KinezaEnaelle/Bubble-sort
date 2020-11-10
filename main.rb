8a95f14e05e20dec8a3d1223c10164536492b622
KinezaEnaelle <kiris2024@gmail.com>
2020-11-10 19:40:14 +0100

    Fixes Requested changes

--- main.rb
+++ main.rb
@@ -12,18 +12,23 @@ def bubble_sort(array)
   end
   array
 end
+ arr = [1,3,4,2,6,5]
+ print bubble_sort(arr)
 
-def bubble_sort_by(array)
-  n = array.length
-  loop do
-    swap = false
-    (n - 1).times do |i|
-      if yield(array[i], array[i + 1]).positive?
-        array[i], array[i + 1] = array[i + 1], array[i]
-        swap = true
+
+ def bubble_sort_by(array)
+    n = array.length
+    loop do
+      swap = false
+      (n - 1).times do |i|
+        if yield(array[i], array[i + 1])
+          array[i], array[i + 1] = array[i + 1], array[i]
+          swap = true
+        end
       end
+      break unless swap
     end
-    break unless swap
+    array
   end
-  array
-end
+  arr = [1,5,3,6,2,4,1]
+  print bubble_sort_by(arr) {|arg1, arg2| arg1.length > arg2.length}

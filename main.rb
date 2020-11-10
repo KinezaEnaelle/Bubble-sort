7cdc3e041b9dda4946d0e622f6db050ee27a4357
Henok <henatan99@gmail.com>
2020-11-10 18:44:34 +0300

    solve file conflict

---
+++ main.rb
@@ -0,0 +1,45 @@
+def bubble_sort(array)
+  n = array.length
+  loop do
+    swap = false
+    (n - 1).times do |i|
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swap = true
+      end
+    end
+    break unless swap
+  end
+  array
+end
+
+arr = [6, 4, 2, 1, 3]
+print bubble_sort(arr)
+
+# rubocop:disable Metrics/AbcSize
+def bubble_sort_by(array)
+  array = []
+  array.each do |elem|
+    array << yield(elem)
+  end
+  n = array.length
+  loop do
+    swap = false
+    (n - 1).times do |i|
+      next unless array[i] > array[i + 1]
+      array[i], array[i + 1] = array[i + 1], array[i]
+      swap = true
+    end
+    break unless swap
+  end
+  array
+end
+# rubocop:enable Metrics/AbcSize
+
+arr = [6, 4, 2, 1, 3]
+bubble_sort_by do |elem|
+  array = arr
+  array 
+end
+
+# rubocop: enable Metrics/ModuleLength

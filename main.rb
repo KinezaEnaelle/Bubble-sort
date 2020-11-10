8052fa73d0fc492f4ae3430cf176fd00afc3679e
Henok <henatan99@gmail.com>
2020-11-10 13:58:25 +0300

    Adds bubble_sort_by method in addition to bubble_sort - Adds bubble_sort_by method incorporating yield method/keyword - The yield in this case is each element in the passed array

--- main.rb
+++ main.rb
@@ -17,3 +17,33 @@ def bubble_sort(array)
   arr = [6,4,2,1,3]
   print bubble_sort(arr)
 
+
+
+
+  def bubble_sort_by  
+
+    array = []
+    
+    self.each do |elem|
+      array << yield(elem)
+    end
+
+    n = array.length
+    i = 0
+    loop do
+      swap = false
+      (n-1).times do|i|
+        if array[i] > array[i+1]
+          array[i], array[i+1] = array[i+1], array[i]
+          swap = true
+        end
+    end
+    break if not swap
+  end
+  array
+  end
+  
+  arr = [6,4,2,1,3] 
+  arr.bubble_sort_by do |elem| 
+    elem
+  end

4dbbc86f54dc4a55a479bd496d62b3c3d88681dc
Henok <henatan99@gmail.com>
2020-11-10 15:14:22 +0300

    Fix more robocop error

--- main.rb
+++ main.rb
@@ -16,7 +16,6 @@ end
 arr = [6, 4, 2, 1, 3]
 print bubble_sort(arr)
 
-# rubocop:disable Metrics/AbcSize
 def bubble_sort_by
   array = []
   each do |elem|
@@ -26,15 +25,15 @@ def bubble_sort_by
   loop do
     swap = false
     (n - 1).times do |i|
-      next unless array[i] > array[i + 1]
-      array[i], array[i + 1] = array[i + 1], array[i]
-      swap = true
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swap = true
+      end
     end
     break unless swap
   end
   array
 end
-# rubocop:enable Metrics/AbcSize
 
 arr = [6, 4, 2, 1, 3]
 arr.bubble_sort_by do |elem|
